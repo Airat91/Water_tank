a5652080dc9298d0eab4b64d12932767c524f77d
unknown <Girfanov.Ayrat@yandex.ru>
2020-04-05 00:18:58 +0500

    add some LCD graphical functions

--- inc/LCD.h
+++ inc/LCD.h
@@ -17,11 +17,22 @@ typedef enum {
     LCD_RS_DATA = 1,
 }LCD_RS_t;
 
+typedef enum {
+    LCD_COLOR_WHITE = 0,
+    LCD_COLOR_BLACK = 1,
+}LCD_color_t;
+
 /**
  * @brief lcd_spi
  * @ingroup LCD
  */
 extern SPI_HandleTypeDef lcd_spi;
+
+/**
+ * @brief LCD_buf
+ * @ingroup LCD
+ */
+extern uint8_t LCD_buf[];
 
 /*========== FUNCTION PROTOTYPES ==========*/
 
@@ -32,5 +43,10 @@ void LCD_spi_deinit (void);
 void LCD_gpio_init (void);
 void LCD_gpio_deinit (void);
 void LCD_send(LCD_RW_t rw, LCD_RS_t rs, uint8_t data);
+void LCD_update(void);
+int LCD_set_pixel(uint8_t x, uint8_t y, LCD_color_t color);
+int LCD_fill_area(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, LCD_color_t color);
+int LCD_invert_area(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2);
+void LCD_clr(void);
 
 #endif // LCD_H
--- src/LCD.c
+++ src/LCD.c
@@ -6,6 +6,7 @@
 #include "stm32f1xx_hal_gpio.h"
 #include "stm32f1xx_hal_spi.h"
 #include "stm32f1xx_hal_rcc.h"
+#include "math.h"
 
 /**
   * @defgroup LCD
@@ -17,11 +18,15 @@
   * @{
   */
 #define LCD_SPI SPI1
+#define MAX_X 128
+#define MAX_Y 64
+#define LCD_BUF_ARRAY_LEN 1024
 /**
   * @}
   */
 
 SPI_HandleTypeDef lcd_spi = {0};
+uint8_t LCD_buf[1024] = {0};
 
 /*========== FUNCTIONS ==========*/
 
@@ -44,63 +49,24 @@ int LCD_init (void){
     HAL_GPIO_WritePin(LCD_RST_PORT,LCD_RST_PIN,GPIO_PIN_SET);
     HAL_Delay(100);
     LCD_send(LCD_RW_WRITE, LCD_RS_COMM, 0x0C);
-    //HAL_Delay(100);
-    LCD_send(LCD_RW_WRITE, LCD_RS_COMM, 0x36);
-    //HAL_Delay(100);
     LCD_send(LCD_RW_WRITE, LCD_RS_COMM, 0x01);
-    //HAL_Delay(100);
-    LCD_send(LCD_RW_WRITE, LCD_RS_COMM, 0x80);
-    LCD_send(LCD_RW_WRITE, LCD_RS_COMM, 0x80);
-    LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-    LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-    LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-    LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-    LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-    LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-    LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-    LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-    LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-    LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-    LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-    LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-    LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-    LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-    LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-    LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
+    HAL_Delay(2);
+    LCD_send(LCD_RW_WRITE, LCD_RS_COMM, 0x36);
 
-    for(uint8_t y = 0; y < 32; y++){
-        for (uint8_t x = 0; x < 8; x++){
-        LCD_send(LCD_RW_WRITE, LCD_RS_COMM, (y + 0x80));
-        LCD_send(LCD_RW_WRITE, LCD_RS_COMM, (x + 0x80));
-        LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-        LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-        }
-    }
-    for(uint8_t y = 0; y < 32; y++){
-        for (uint8_t x = 8; x < 16; x++){
-        LCD_send(LCD_RW_WRITE, LCD_RS_COMM, (y + 0x80));
-        LCD_send(LCD_RW_WRITE, LCD_RS_COMM, (x + 0x80));
-        LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-        LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0xFF);
-        }
-    }
+    LCD_clr();
+    LCD_update();
+    HAL_Delay(500);
 
-    for(uint8_t y = 0; y < 32; y++){
-        for (uint8_t x = 0; x < 8; x++){
-        LCD_send(LCD_RW_WRITE, LCD_RS_COMM, (y + 0x80));
-        LCD_send(LCD_RW_WRITE, LCD_RS_COMM, (x + 0x80));
-        LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0x00);
-        LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0x00);
-        }
+    uint8_t y = 0;
+    for(uint8_t x = 0; x < 128; x++){
+        y = (uint8_t)(20*sin((double)x*0.17) + 32);
+        LCD_set_pixel(x,y,LCD_COLOR_BLACK);
     }
-    for(uint8_t y = 0; y < 32; y++){
-        for (uint8_t x = 8; x < 16; x++){
-        LCD_send(LCD_RW_WRITE, LCD_RS_COMM, (y + 0x80));
-        LCD_send(LCD_RW_WRITE, LCD_RS_COMM, (x + 0x80));
-        LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0x00);
-        LCD_send(LCD_RW_WRITE, LCD_RS_DATA, 0x00);
-        }
-    }
+    LCD_update();
+    LCD_fill_area(60,55,80,63,LCD_COLOR_BLACK);
+    LCD_update();
+    LCD_invert_area(30,30,100,63);
+    LCD_update();
     return result;
 }
 /**
@@ -127,7 +93,7 @@ int LCD_spi_init (void){
     lcd_spi.Init.CLKPolarity = SPI_POLARITY_LOW;
     lcd_spi.Init.CLKPhase = SPI_PHASE_1EDGE;
     lcd_spi.Init.NSS = SPI_NSS_SOFT;
-    lcd_spi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
+    lcd_spi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_128;
     lcd_spi.Init.FirstBit = SPI_FIRSTBIT_MSB;
     lcd_spi.Init.TIMode = SPI_TIMODE_DISABLE;
     lcd_spi.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
@@ -164,7 +130,6 @@ void LCD_gpio_init (void){
     GPIO_InitStruct.Pin = LCD_CS_PIN;
     HAL_GPIO_Init(LCD_CS_PORT, &GPIO_InitStruct);
     GPIO_InitStruct.Pin = LCD_RST_PIN;
-    //HAL_GPIO_WritePin(LCD_RST_PORT, LCD_RST_PIN, GPIO_PIN_SET);
     HAL_GPIO_Init(LCD_RST_PORT, &GPIO_InitStruct);
 
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
@@ -202,7 +167,6 @@ void LCD_gpio_deinit (void){
  * @ingroup LCD
  */
 void LCD_send(LCD_RW_t rw, LCD_RS_t rs, uint8_t data){
-    HAL_StatusTypeDef error = HAL_OK;
     uint8_t array[3] = {0xF8,0x00,0x00};
     array[0] |= (uint8_t)rw << 2;
     array[0] |= (uint8_t)rs << 1;
@@ -213,9 +177,177 @@ void LCD_send(LCD_RW_t rw, LCD_RS_t rs, uint8_t data){
     lcd_spi.Instance = LCD_SPI;
 
     HAL_GPIO_WritePin(LCD_CS_PORT, LCD_CS_PIN,GPIO_PIN_SET);
-    error = HAL_SPI_Transmit(&lcd_spi,&array[0],3,100);
+    HAL_SPI_Transmit(&lcd_spi,&array[0],3,100);
     while(((uint16_t)_lcd_spi->SR & SPI_SR_BSY)){
     }
     HAL_GPIO_WritePin(LCD_CS_PORT, LCD_CS_PIN,GPIO_PIN_RESET);
-    //HAL_Delay(1);
+}
+/**
+ * @brief Update data from LCD_buf to screen
+ * @ingroup LCD
+ */
+void LCD_update(void){
+    uint8_t y = 0;
+    for(uint16_t i = 0; i < LCD_BUF_ARRAY_LEN; i++){
+        if(i%32 == 0){
+            LCD_send(LCD_RW_WRITE, LCD_RS_COMM, (y + 0x80));
+            LCD_send(LCD_RW_WRITE, LCD_RS_COMM, (0x80));
+            y++;
+        }
+        LCD_send(LCD_RW_WRITE, LCD_RS_DATA, LCD_buf[i]);
+    }
+}
+/**
+ * @brief Set pixel in LCD_buf
+ * @param x - horizontal position = {0..127}
+ * @param y - vertical position = {0...63}
+ * @param color - pixel's color = {LCD_COLOR_BLACK, LCD_COLOR_WHITE}
+ * @ingroup LCD
+ * @return  0 - OK,\n
+ *          -1 - x or y out of range
+ */
+int LCD_set_pixel(uint8_t x, uint8_t y, LCD_color_t color){
+    int result = 0;
+    if((x > MAX_X) || (y > MAX_Y)){
+        result = -1;
+    }else{
+        y = 63 - y;
+        uint8_t x_pos = x/8;
+        uint8_t x_shift = x%8;
+        if(y > 31){
+            x_pos += 16;
+            y -= 32;
+        }
+        if(color == LCD_COLOR_BLACK){
+            LCD_buf[y*32 + x_pos] |= (0x80 >> x_shift);
+        }else{
+            LCD_buf[y*32 + x_pos] &= ~(0x80 >> x_shift);
+        }
+    }
+    return result;
+}
+/**
+ * @brief Fills rectangular area by selected color
+ * @param x1 - first corner's horisonal position = {0..127}
+ * @param y1 - first corner's vertical position = {0...63}
+ * @param x2 - second corner's horisonal position = {0..127}
+ * @param y2 - second corner's vertical position = {0...63}
+ * @param color - pixel's color = {LCD_COLOR_BLACK, LCD_COLOR_WHITE}
+ * @ingroup LCD
+ * @return  0 - OK,\n
+ *          -1 - x1 out of range
+ *          -2 - y1 out of range
+ *          -3 - x2 out of range
+ *          -4 - y2 out of range
+ *
+ * Automatically corrects x, y values if out of range,\n
+ */
+int LCD_fill_area(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, LCD_color_t color){
+    int result = 0;
+    //check coordinates values
+    if(x1 > MAX_X){
+        x1 = MAX_X;
+        result = -1;
+    }else if(y1 > MAX_Y){
+        y1 = MAX_Y;
+        result = -2;
+    }else if(x2 > MAX_X){
+        x2 = MAX_X;
+        result = -3;
+    }else if(y1 > MAX_Y){
+        y2 = MAX_Y;
+        result = -4;
+    }
+    // sort coordinates ascending
+    if(x1 > x2){
+        uint8_t temp = x2;
+        x2 = x1;
+        x1 = temp;
+    }
+    if(y1 > y2){
+        uint8_t temp = y2;
+        y2 = x1;
+        y1 = temp;
+    }
+    // fill LCD_buf
+    for (uint8_t y = y1; y <= y2; y++) {
+        for (uint8_t x = x1; x <= x2; x++) {
+            LCD_set_pixel(x,y,color);
+        }
+    }
+    return result;
+}
+/**
+ * @brief Fill LCD_buf by 0x00 (white pixels)
+ * @ingroup LCD
+ */
+void LCD_clr(void){
+    for (uint16_t i = 0; i < LCD_BUF_ARRAY_LEN; i++) {
+        LCD_buf[i] = 0x00;
+    }
+}
+/**
+ * @brief Invert pixels color in set area
+ * @param x1 - first corner's horisonal position = {0..127}
+ * @param y1 - first corner's vertical position = {0...63}
+ * @param x2 - second corner's horisonal position = {0..127}
+ * @param y2 - second corner's vertical position = {0...63}
+ * @param color - pixel's color = {LCD_COLOR_BLACK, LCD_COLOR_WHITE}
+ * @ingroup LCD
+ * @return  0 - OK,\n
+ *          -1 - x1 out of range
+ *          -2 - y1 out of range
+ *          -3 - x2 out of range
+ *          -4 - y2 out of range
+ *
+ * Automatically corrects x, y values if out of range,\n
+ */
+int LCD_invert_area(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2){
+    int result = 0;
+    uint8_t y_tmp = 0;
+    uint8_t x_pos = 0;
+    uint8_t x_shift = 0;
+    //check coordinates values
+    if(x1 > MAX_X){
+        x1 = MAX_X;
+        result = -1;
+    }else if(y1 > MAX_Y){
+        y1 = MAX_Y;
+        result = -2;
+    }else if(x2 > MAX_X){
+        x2 = MAX_X;
+        result = -3;
+    }else if(y1 > MAX_Y){
+        y2 = MAX_Y;
+        result = -4;
+    }
+    // sort coordinates ascending
+    if(x1 > x2){
+        uint8_t temp = x2;
+        x2 = x1;
+        x1 = temp;
+    }
+    if(y1 > y2){
+        uint8_t temp = y2;
+        y2 = x1;
+        y1 = temp;
+    }
+    // fill LCD_buf
+    for (uint8_t y = y1; y <= y2; y++) {
+        for (uint8_t x = x1; x <= x2; x++) {
+            y_tmp = 63 - y;
+            x_pos = x/8;
+            x_shift = x%8;
+            if(y_tmp > 31){
+                x_pos += 16;
+                y_tmp -= 32;
+            }
+            if(LCD_buf[y_tmp*32 + x_pos] & (0x80 >> x_shift)){
+                LCD_set_pixel(x,y,LCD_COLOR_WHITE);
+            }else{
+                LCD_set_pixel(x,y,LCD_COLOR_BLACK);
+            }
+        }
+    }
+    return result;
 }
